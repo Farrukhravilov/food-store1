5f93706b54cf20b788ee380f90cdb52f59be34da
Garant <azamov0404@gmai.com>
2024-04-20 01:05:21 +0300

    Добавил главную страницу сверстал шапку сайта

---
+++ src/js/main.js
@@ -0,0 +1,56 @@
+const myObj = {
+    queue: [],
+    init: function () {
+        let queue = this.queue;
+
+        for (key in queue) {
+            let f = queue[key];
+            if (typeof f == 'function') {
+                f();
+            }
+        }
+    }
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    myObj.init();
+});
+
+
+myObj.queue = {
+
+    menuTarger: function name(params) {
+        var currentUrl = window.location.href;
+
+        var menuLinks = document.querySelectorAll('.header-menu__link');
+
+        menuLinks.forEach(function (link) {
+            if (link.href === currentUrl) {
+                link.classList.add('activ');
+            }
+        });
+    },
+
+    searchOpen: function name(params) {
+        document.addEventListener('DOMContentLoaded', function () {
+            const inputField = document.querySelector('.site-search__panel');
+            const searchButton = document.getElementById('searchBtn');
+
+            function toggleButtonState() {
+                if (inputField.value.trim() === '') {
+                    searchButton.classList.add('disabled');
+                } else {
+                    searchButton.classList.remove('disabled');
+                }
+            }
+
+            function addActivClass() {
+                inputField.classList.add('activ');
+            }
+
+            searchButton.addEventListener('click', addActivClass);
+            inputField.addEventListener('input', toggleButtonState);
+            toggleButtonState(); // Initial state check
+        });
+    }
+}
